8e6ccc73a92cb7fa988f0e92eac4c6c48d8c4e26
Ege_Perim <perim.ege@gmail.com>
2023-10-09 21:35:39 +0300

    exception added

--- RecursiveFunction/src/Main.java
+++ RecursiveFunction/src/Main.java
@@ -1,11 +1,16 @@
+import java.security.InvalidParameterException;
+
 public class Main {
     static int iterationCount=0;
     public static void main(String[] args) {
-        System.out.println(findExponent(3,10));
+        System.out.println(findExponent(-3,-4));
         System.out.println("Iteration count is:"+iterationCount);
     }
 
     public static int findExponent(int base,int exponent){
+        if(exponent<0){
+            throw new InvalidParameterException("Exponent have to be positive!");
+        }
         iterationCount++;
         if(exponent==0){
             return 1;
